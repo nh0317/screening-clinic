d58e778bf2f67433d1405aaa3c8ac956dc9fb16d
KNH <nh2004@naver.com>
2022-02-20 13:12:19 +0900

    fix : 버그 수정

--- src/main/java/com/reservation/screeningclinic/controller/QuestionnaireController.java
+++ src/main/java/com/reservation/screeningclinic/controller/QuestionnaireController.java
@@ -1,17 +1,21 @@
 package com.reservation.screeningclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class QuestionnaireController {
-    @GetMapping("/questionnaire")
-    public String getQuestionnaire(){
+    @GetMapping("/questionnaire/{screenClinicIdx}")
+    public String getQuestionnaire(@PathVariable("screenClinicIdx")Long screenClinicIdx, Model model){
+        model.addAttribute("screenClinicIdx", screenClinicIdx);
         return "문진표2";
     }
 
-    @GetMapping("/questionnaire2")
-    public String getQuestionnaire2(){
+    @GetMapping("/questionnaire2/{screenClinicIdx}")
+    public String getQuestionnaire2(@PathVariable("screenClinicIdx")Long screenClinicIdx, Model model){
+        model.addAttribute("screenClinicIdx", screenClinicIdx);
         return "문진표3";
     }
 }
